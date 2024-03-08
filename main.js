3c9588d21df6548d0356c33aceac23a99f0fcdba
Shevyakov <shevyakoved.mer.ci.nsu.ru>
2024-03-08 20:45:18 +0700

    Add filters

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 let elements = document.querySelectorAll('.element');
 let list = document.querySelector('.list');
+let completedTasksButton = document.querySelector('.completedtasks');
+let uncompletedTasksButton = document.querySelector('.uncompletedtasks');
+let allTasksButton = document.querySelector('.alltask');
 let color = 135;
 
 function addRemoveHandler(element) {
@@ -10,10 +13,12 @@ function addRemoveHandler(element) {
         element.remove();
         elements = document.querySelectorAll('.element');
         color -= 30;
+        updateTaskCount();
     });
 
     complete.addEventListener('click', () => {
         element.classList.toggle('completed');
+        updateTaskCount();
     });
 }
 
@@ -32,9 +37,14 @@ let input = document.querySelector('.newElementInput');
 let clearButton = document.querySelector('.clear');
 
 clearButton.addEventListener('click', () => {
+    clearAllTasks()
+});
+
+function clearAllTasks() {
     list.innerHTML = '';
     color = 135;
-});
+    updateTaskCount();
+}
 
 createButton.addEventListener('click', () => {
     let inputText = input.value.trim();
@@ -50,5 +60,82 @@ createButton.addEventListener('click', () => {
         elements.forEach(element => {
             addRemoveHandler(element);
         });
+
+        updateTaskCount();
     }
 });
+
+completedTasksButton.addEventListener('click', () => {
+    filterTasks(true);
+    removeBold(uncompletedTasksButton);
+    removeBold(allTasksButton);
+    toggleBold(completedTasksButton);
+});
+
+uncompletedTasksButton.addEventListener('click', () => {
+    filterTasks(false);
+    removeBold(completedTasksButton);
+    removeBold(allTasksButton);
+    toggleBold(uncompletedTasksButton);
+});
+
+allTasksButton.addEventListener('click', () => {
+    showAllTasks();
+    removeBold(completedTasksButton);
+    removeBold(uncompletedTasksButton);
+    toggleBold(allTasksButton);
+});
+
+function toggleBold(button) {
+    button.classList.toggle('bold');
+}
+
+function removeBold(button) {
+    button.classList.remove('bold');
+}
+
+function filterTasks(completed) {
+    elements.forEach(element => {
+        if (completed && !element.classList.contains('completed')) {
+            element.classList.add('hidden');
+        } else if (!completed && element.classList.contains('completed')) {
+            element.classList.add('hidden');
+        } else {
+            element.classList.remove('hidden');
+        }
+    });
+}
+
+function showAllTasks() {
+    elements.forEach(element => {
+        element.classList.remove('hidden');
+    });
+}
+
+function updateTaskCount() {
+    let completedCount = 0;
+    let uncompletedCount = 0;
+
+    elements.forEach(element => {
+        if (element.classList.contains('completed')) {
+            completedCount++;
+        } else {
+            uncompletedCount++;
+        }
+    });
+
+
+    let totalCount = elements.length;
+
+    if (list.innerHTML == '') {
+        completedCount = 0;
+        uncompletedCount = 0;
+        totalCount = 0;
+    }
+
+    completedTasksButton.textContent = `Сделано(${completedCount})`;
+    uncompletedTasksButton.textContent = `Не сделано(${uncompletedCount})`;
+    allTasksButton.textContent = `Все(${totalCount})`;
+}
+
+updateTaskCount();
